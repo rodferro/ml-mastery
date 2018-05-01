01b17e015c5f110a987c9f9ae9a0b305ee5b75ca
rodferro <rod_ferro@msn.com>
2018-05-01 10:34:45 -0300

    Add new python script

---
+++ 05_data_types.py
@@ -0,0 +1,7 @@
+# Data types for each attribute
+from pandas import read_csv
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+print(data.dtypes)
