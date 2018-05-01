d667114008c1474d91a332f00507345edd8a8c15
rodferro <rod_ferro@msn.com>
2018-05-01 17:14:46 -0300

    Add new python script

---
+++ 06_scatterplot_matrix.py
@@ -0,0 +1,10 @@
+# Scatterplot matrix
+from matplotlib import pyplot
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+
+filename = "pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+scatter_matrix(data)
+pyplot.show()
