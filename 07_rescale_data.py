17f651761df75dee103a836e3183c2266b411815
rodferro <rod_ferro@msn.com>
2018-05-02 20:23:00 -0300

    Add new python script

---
+++ 07_rescale_data.py
@@ -0,0 +1,19 @@
+# Rescale data between 0 and 1
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn.preprocessing import MinMaxScaler
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+array = data.values
+
+# separate array into input and output components
+X = array[:, 0:8]
+y = array[:, 8]
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaledX = scaler.fit_transform(X)
+
+# summarize transformed data
+set_printoptions(precision=3)
+print(rescaledX[0:5, :])
