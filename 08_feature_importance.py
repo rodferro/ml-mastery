d51354abaaf275f850d2e30b93950485a936300a
rodferro <rod_ferro@msn.com>
2018-05-04 21:53:07 -0300

    Add new python script

---
+++ 08_feature_importance.py
@@ -0,0 +1,15 @@
+# Feature importance with Extra Trees Classifier
+from pandas import read_csv
+from sklearn.ensemble import ExtraTreesClassifier
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class'] 
+data = read_csv(filename, names=names)
+array = data.values
+X = array[:, 0:8]
+y = array[:, 8]
+
+# feature extraction
+model = ExtraTreesClassifier()
+model.fit(X, y)
+print(model.feature_importances_)
---
+++ 08_pca.py
@@ -0,0 +1,18 @@
+# Feature extraction with PCA
+from pandas import read_csv
+from sklearn.decomposition import PCA
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+array = data.values
+X = array[:, 0:8]
+y = array[:, 8]
+
+# feature extraction
+pca = PCA(n_components=3)
+fit = pca.fit(X)
+
+# summarize components
+print("Explained variance: %s" % (fit.explained_variance_ratio_))
+print(fit.components_)
