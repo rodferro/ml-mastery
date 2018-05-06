ae6e197236936a5a16526be3a631970c17f91b32
rodferro <rod_ferro@msn.com>
2018-05-06 16:52:26 -0300

    Add new python script

---
+++ 09_loocv.py
@@ -0,0 +1,16 @@
+# Evaluate using Leave One Out Cross Validation
+from pandas import read_csv
+from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+array = data.values
+X = array[:, 0:8]
+y = array[:, 8]
+loocv = LeaveOneOut()
+model = LogisticRegression()
+results = cross_val_score(model, X, y, cv=loocv)
+print('Accuracy: %.3f%% (%.3f%%)' % (results.mean() * 100.0, results.std() * 100.0))
