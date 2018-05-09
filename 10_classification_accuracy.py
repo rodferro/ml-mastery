c4afe1ef8e37db09d49bc51d60cb28d5c60e012e
rodferro <rod_ferro@msn.com>
2018-05-08 22:04:44 -0300

    Add new python script

---
+++ 10_classification_accuracy.py
@@ -0,0 +1,16 @@
+# Cross validation classification accuracy
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = read_csv(filename, names=names)
+array = data.values
+X = array[:, 0:8]
+y = array[:, 8]
+kfold = KFold(n_splits=10, random_state=7)
+model = LogisticRegression()
+results = cross_val_score(model, X, y, cv=kfold, scoring='accuracy')
+print('Accuracy: %.3f (%.3f)' % (results.mean(), results.std()))
