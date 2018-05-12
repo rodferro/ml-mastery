09640ce5e8760473818c50651bef2689a3d1badc
rodferro <rod_ferro@msn.com>
2018-05-12 16:44:47 -0300

    Add new python script

---
+++ 12_k_nearest_neighbors.py
@@ -0,0 +1,17 @@
+# KNN regression
+from pandas import read_csv
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.neighbors import KNeighborsRegressor
+
+filename = 'housing.csv'
+names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO',
+'B', 'LSTAT', 'MEDV']
+data = read_csv(filename, delim_whitespace=True, names=names)
+array = data.values
+X = array[:, :13]
+y = array[:, 13]
+kfold = KFold(n_splits=10, random_state=7)
+model = KNeighborsRegressor()
+results = cross_val_score(model, X, y, cv=kfold, scoring='neg_mean_squared_error')
+print(results.mean())
